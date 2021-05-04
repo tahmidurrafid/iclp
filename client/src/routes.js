16603eca0ea6600abf22b5b15a8654f54e3adec8
unknown <nazrinshukti@gmail.com>
2021-05-04 23:35:56 +0600

    done instructor dashboard partially,will do create-course

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,11 @@
 import CourseView from './components/CourseView.vue'
 import CourseDetails from './components/CourseDetails.vue'
 import CreateThread from './components/CreateThread.vue'
+import InstructorDashboard from './components/InstructorDashboard'
 
 export default [
    { path : '/course', component: CourseView},
    { path : '/course/details', component: CourseDetails},
    { path : '/createthread', component: CreateThread},
+   { path : '/instructordashboard', component: InstructorDashboard},
 ];
